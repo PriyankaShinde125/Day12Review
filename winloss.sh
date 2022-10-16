0fb3f119c5484401df98a62633ecc539c7cddc33
“PriyankaShinde” <pkshinde125@gmail.com>
2022-10-16 10:42:51 +0530

    [Priyanka] review win loss solution added

---
+++ winloss.sh
@@ -0,0 +1,32 @@
+#! /bin/bash -x
+declare -A result
+result['win']=0
+result['loss']=0
+ishead=1
+toss=0
+noOfPlays=0
+i=1
+percentageOfWins=0
+
+echo "enter how many times do you want to play"
+read noOfPlays
+
+while [ $i -le $noOfPlays ]
+do
+	toss=$((RANDOM%2))
+	if [[ $toss -eq $ishead ]]
+	then
+		result['win']=$((${result['win']}+1))
+	else
+		result['loss']=$((${result['loss']}+1))
+	fi
+	((i++))
+done
+
+
+echo "wins=${result['win']}"
+echo "loss=${result['loss']}"
+
+percentageOfWins=`echo ${result['win']} $noOfPlays | awk '{print ($1/$2*100)}'`
+
+echo "percentage of wins= $percentageOfWins"
